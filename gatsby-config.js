26b614fbb602f73424145bd4b210f842aadfd447
siner308 <siner308@gmail.com>
2020-11-28 04:16:27 +0900

    fix configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    siteName: `Siner's DevLog`,
-    githubUrl: `https://github.com/siner308`,
+    siteName: "Siner's Blog",
+    githubUrl: "https://github.com/siner308",
   },
   plugins: [
     // `gatsby-plugin-typescript` is automatically included in gatsby
     // You only need to explicitly define it here if you need to configure
     // specific options in it
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: `src/utils/typography.js`,
+        pathToConfigModule: "src/utils/typography",
         omitGoogleFont: true,
       },
     },
