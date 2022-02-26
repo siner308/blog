7a1ab9edb28269f7509418adc73e79a36fe19e59
siner308 <siner308@gmail.com>
2022-02-26 21:59:55 +0900

    post 카드 전체를 클릭 가능하도록 수정

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,39 +7,35 @@ import { backgroundStyle } from '../style/backgroundStyle';
 const Post = ({ node }: { node: RemarkableFileSystemNode }): JSX.Element => {
   const { childMarkdownRemark } = node;
   const dateJson: DateJson = getDateJson(node.childMarkdownRemark.frontmatter.date);
+  // TODO: https://sohee1702.tistory.com/117?category=788998 포스트잇 효과도 이뻐보인다
+  // TODO: https://post.naver.com/viewer/postView.nhn?volumeNo=7781411 이것도 이쁘다
   return (
-    <div
-      // TODO: https://sohee1702.tistory.com/117?category=788998 포스트잇 효과도 이뻐보인다
-      // TODO: https://post.naver.com/viewer/postView.nhn?volumeNo=7781411 이것도 이쁘다
+    <Link
+      to={getPostPath(node.childMarkdownRemark.frontmatter.date, node.name)}
       style={{
-        ...backgroundStyle,
-        filter: 'drop-shadow(0px 4px 12px rgba(0, 0, 0, 0.25))',
+        ...backgroundStyle, filter: 'drop-shadow(0px 4px 12px rgba(0, 0, 0, 0.25))',
+        textDecoration: 'none', color: 'black',
       }}
     >
-      <Link
-        to={getPostPath(node.childMarkdownRemark.frontmatter.date, node.name)}
-        style={{ textDecoration: 'none', color: 'black' }}
+      <div
+        style={{
+          marginRight: 10,
+          marginLeft: 10,
+          paddingTop: 10,
+          paddingBottom: 10,
+        }}
       >
-        <div
-          style={{
-            marginRight: 10,
-            marginLeft: 10,
-            paddingTop: 10,
-            paddingBottom: 10,
-          }}
-        >
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <p style={{ fontWeight: 'bold' }}>{childMarkdownRemark.frontmatter.title}</p>
-            <br />
-            {/*<span>{`${dateJson.year}년 ${dateJson.month}월 ${dateJson.day}일`}</span>*/}
-          </div>
-          {childMarkdownRemark.frontmatter.subtitle && (
-            <div style={{ fontWeight: 'normal' }}>{childMarkdownRemark.frontmatter.subtitle}</div>
-          )}
-          <div style={{ fontWeight: 'lighter', fontSize: 'small' }}>{childMarkdownRemark.excerpt}</div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <p style={{ fontWeight: 'bold' }}>{childMarkdownRemark.frontmatter.title}</p>
+          <br/>
+          {/*<span>{`${dateJson.year}년 ${dateJson.month}월 ${dateJson.day}일`}</span>*/}
         </div>
-      </Link>
-    </div>
+        {childMarkdownRemark.frontmatter.subtitle && (
+          <div style={{ fontWeight: 'normal' }}>{childMarkdownRemark.frontmatter.subtitle}</div>
+        )}
+        <div style={{ fontWeight: 'lighter', fontSize: 'small' }}>{childMarkdownRemark.excerpt}</div>
+      </div>
+    </Link>
   );
 };
 
