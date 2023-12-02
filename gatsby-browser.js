35c2a048050a1e70d11418e1aa5d119895945481
Jeonghyun An <siner308@gmail.com>
2023-12-02 17:03:52 +0900

    code block font to monospace (#57)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,4 +3,4 @@ require('/stylesheets/table-of-contents.css');
 require('/stylesheets/quote.css');
 require('/stylesheets/post-grid.css');
 require('/stylesheets/post-card.css');
-require('/stylesheets/code.css');
+require('/stylesheets/global.css');
